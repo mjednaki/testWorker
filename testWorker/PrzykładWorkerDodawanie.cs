133d0cd57eccd5595fffe17cc27c071b216eca13
Michał Jednaki <mjednaki@glc.pl>
2024-09-13 20:44:24 +0200

    Działający przykład dodawania załącznika na tupla

--- testWorker/PrzykładWorkerDodawanie.cs
+++ testWorker/PrzykładWorkerDodawanie.cs
@@ -7,6 +7,7 @@ using Soneta.Towary;
 using Soneta.Types;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,19 +28,37 @@ namespace testWorker
         {
             Session Session = Context.Session;
             Operator op = Session.Login.Operator;
+            DbTuple tuple = null;
 
-            using (var t = Context.Session.Logout(true)) 
+            using (Session newSession = Session.Login.CreateSession(false, false)) 
             {
-                var cm = CoreModule.GetInstance(Session);
-                DbTupleDefinition def = cm.TuplesDefs.WgNazwa["Operators","Załączniki"];
-                DbTuple tuple = def.CreateRow(op);
-                cm.Tuples.AddRow(tuple);
-                tuple.Fields["email"] = "adres@wp.pl";
-                tuple.Fields["Data"] = Date.Today;
-                t.CommitUI();
+                using (var tr = newSession.Logout(true))
+                {
+                    var cm = CoreModule.GetInstance(newSession);
 
-                //Attachment att = new Attachment(tuple, AttachmentType.Attachments);
-                //att.AttachmentFile
+                    DbTupleDefinition def = cm.TuplesDefs.WgNazwa["Operators", "Załączniki"];
+                    tuple = def.CreateRow(op);
+                    cm.Tuples.AddRow(tuple);
+                    tuple.Fields["email"] = "adres@wp.pl";
+                    tuple.Fields["Data"] = Date.Today;
+                    tr.CommitUI();
+                }
+                newSession.Save();
+            }
+
+
+            using (var t = Session.Logout(true))
+            {
+                DbTuple tupel = CoreModule.GetInstance(Session).Tuples[tuple.Guid] ;
+                Attachment att = new Attachment(tupel, AttachmentType.Attachments);
+                BusinessModule.GetInstance(Session).Attachments.AddRow(att);
+                att.Name = "Test";
+
+                Stream fs = File.OpenRead(@"C:\enovaNET\1.pdf");
+
+                att.LoadFromStream(fs);
+
+                t.CommitUI();
             }
 
         }
